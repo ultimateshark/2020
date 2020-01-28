847625946b8ca0c691eab73ff20fad0aa1be0161
ultimateshark <ultimateshark@localhost.localdomain>
2020-01-28 22:00:52 +0530

    update

--- Rann 20/init.py
+++ Rann 20/init.py
@@ -234,7 +234,7 @@ def check_part():
 
 @app.route("/user/<string:path>",methods=["GET","POST"])
 def User_Profile(path):
-	try:
+	# try:
 		return render_template('main.html',logged_in=True)
 		if session["logged_in"]:
 			client_email=session["username"]
@@ -1283,8 +1283,8 @@ def User_Profile(path):
 						return str(e)
 		else:
 			return redirect(url_for("Loginpage"))
-	except Exception as e:
-		return render_template("errorpage.html",error="Some Error Occured")
+	# except Exception as e:
+	# 	return render_template("errorpage.html",error="Some Error Occured")
 
 
 @app.route("/loginpage/<string:redir>")
